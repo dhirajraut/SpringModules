133413b905588c3609ea47af13ab04f54c562dd9
Dhiraj Raut <Dhiraj.Raut@nice.com>
2020-01-16 21:12:52 +0530

    Integrated Lombok

--- SimpleSpringBootWebApp/src/main/java/com/galaxy/spring/model/Product.java
+++ SimpleSpringBootWebApp/src/main/java/com/galaxy/spring/model/Product.java
@@ -1,36 +1,14 @@
 package com.galaxy.spring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Product {
 	int id;
 	String name;
 	double amount;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public Product(final int id, final String name, final double amount) {
-		this.name = name;
-		this.amount = amount;
-	}
 }
